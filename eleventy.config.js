77575b3cf7cf1952668fae3bda289ee4027169ca
mig678 <146990411+Mig678@users.noreply.github.com>
2026-07-31 20:41:50 -0700

    Set up Eleventy build pipeline

---
+++ eleventy.config.js
@@ -0,0 +1,17 @@
+/** @type {import("@11ty/eleventy").UserConfig} */
+export default async function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("favicon.svg");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("media");
+  eleventyConfig.addPassthroughCopy("js");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
+    },
+  };
+}
